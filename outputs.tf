78a669f02761220cef2aec85d0853e2382c482ac
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "network_connection_monitors" {
-  description = "All network_connection_monitor resources"
-  value       = azurerm_network_connection_monitor.network_connection_monitors
-}
 output "network_connection_monitors_endpoint" {
-  description = "List of endpoint values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.endpoint]
+  description = "Map of endpoint values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.endpoint }
 }
 output "network_connection_monitors_location" {
-  description = "List of location values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.location]
+  description = "Map of location values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.location }
 }
 output "network_connection_monitors_name" {
-  description = "List of name values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.name]
+  description = "Map of name values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.name }
 }
 output "network_connection_monitors_network_watcher_id" {
-  description = "List of network_watcher_id values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.network_watcher_id]
+  description = "Map of network_watcher_id values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.network_watcher_id }
 }
 output "network_connection_monitors_notes" {
-  description = "List of notes values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.notes]
+  description = "Map of notes values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.notes }
 }
 output "network_connection_monitors_output_workspace_resource_ids" {
-  description = "List of output_workspace_resource_ids values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.output_workspace_resource_ids]
+  description = "Map of output_workspace_resource_ids values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.output_workspace_resource_ids }
 }
 output "network_connection_monitors_tags" {
-  description = "List of tags values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.tags]
+  description = "Map of tags values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.tags }
 }
 output "network_connection_monitors_test_configuration" {
-  description = "List of test_configuration values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.test_configuration]
+  description = "Map of test_configuration values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.test_configuration }
 }
 output "network_connection_monitors_test_group" {
-  description = "List of test_group values across all network_connection_monitors"
-  value       = [for k, v in azurerm_network_connection_monitor.network_connection_monitors : v.test_group]
+  description = "Map of test_group values across all network_connection_monitors, keyed the same as var.network_connection_monitors"
+  value       = { for k, v in azurerm_network_connection_monitor.network_connection_monitors : k => v.test_group }
 }
 
